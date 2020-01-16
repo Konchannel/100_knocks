db1cea81bebbcc94fbcd410608787f4e2cafa472
Konchannel <konchan2416@gmail.com>
2020-01-16 22:41:27 +0900

    add: comment to 03

--- 00_to_09.py
+++ 00_to_09.py
@@ -41,6 +41,9 @@ print(str02)
 03. 円周率
 "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
 という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
+==============================
+point:
+ピリオドなど、正規表現でエスケープが必要な文字はなんとなくでいいから覚えておきたい
 """
 import re
 
