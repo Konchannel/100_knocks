3c81639e6256e711e9d5f39d40696172b80b48d8
Konchannel <konchan2416@gmail.com>
2020-01-29 22:19:32 +0900

    update:solve 18

--- 10_to_19.py
+++ 10_to_19.py
@@ -130,7 +130,7 @@ with codecs.open(temp_text, 'r', 'utf-8') as ht:
 1列目の文字列の種類（異なる文字列の集合）を求めよ．確認にはsort, uniqコマンドを用いよ．
 """
 
-ht_lines = read_hightemp()
+# ht_lines = read_hightemp()
 
 for ht_line in ht_lines:
     print(set(ht_line))
@@ -140,6 +140,15 @@ for ht_line in ht_lines:
 18. 各行を3コラム目の数値の降順にソート
 各行を3コラム目の数値の逆順で整列せよ（注意: 各行の内容は変更せずに並び替えよ）．確認にはsortコマンドを用いよ（この問題はコマンドで実行した時の結果と合わなくてもよい）．
 """
+
+# ht_lines = read_hightemp()
+ht_columns = []
+import re
+
+for index, ht in enumerate(ht_lines):
+    ht_columns.append(list(re.split('   | |\t', ht.rstrip())))
+
+print(sorted(ht_columns, key=lambda ht_val: ht_val[2]))
 
 
 """
